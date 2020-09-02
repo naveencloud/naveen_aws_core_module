8bd9dd341b1246688756707c37609b940646867a
naveen <naveen.kumar@tui.co.uk>
2020-09-02 22:54:42 +0530

    Updated resource

--- launchtemplate/output.tf
+++ launchtemplate/output.tf
@@ -1,9 +1,9 @@
 # Exported Output
 
 output "lt_id" {
-  value = "aws_launch_template.core_launch_template.id"
+  value = aws_launch_template.core_launch_template.id
 }
 
 output "lt_version" {
-  value = "aws_launch_template.core_launch_template.latest_version"
+  value = aws_launch_template.core_launch_template.latest_version
 }
