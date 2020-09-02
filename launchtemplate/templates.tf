5b31abdc5f30f186bc84586d6ddafaca782e74ee
naveen <naveen.kumar@tui.co.uk>
2020-09-02 21:06:33 +0530

    Updated resource

--- launchtemplate/templates.tf
+++ launchtemplate/templates.tf
@@ -1,7 +1,7 @@
 data "template_file" "asgsignal_lt_userdata" {
   template = file("${path.module}/files/userdata_asgsignal.sh")
 
-  vars {
+  vars = {
     cf_StackName      = var.cf_StackName
     region            = var.region
 	cf_resource_id    = var.cf_resource_id
